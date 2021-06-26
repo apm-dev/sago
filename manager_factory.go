31e5ccb80504edc2cd6d504188e936846c90d57c
A.Parsa <develop.apm@gmail.com>
2021-06-26 17:05:29 +0430

    Add saga instance & manager factories

---
+++ manager_factory.go
@@ -0,0 +1,38 @@
+package sago
+
+import (
+	"apm-dev/sago/commands"
+	"apm-dev/sago/messaging"
+)
+
+type SagaManagerFactory struct {
+	sagaInstanceRepository SagaInstanceRepository
+	commandProducer        commands.CommandProducer
+	messageConsumer        messaging.MessageConsumer
+	sagaCommandProducer    SagaCommandProducer
+}
+
+func NewSagaManagerFactory(
+	saga Saga,
+	sagaInstanceRepository SagaInstanceRepository,
+	commandProducer commands.CommandProducer,
+	messageConsumer messaging.MessageConsumer,
+	sagaCommandProducer SagaCommandProducer,
+) *SagaManagerFactory {
+	return &SagaManagerFactory{
+		sagaInstanceRepository: sagaInstanceRepository,
+		commandProducer:        commandProducer,
+		messageConsumer:        messageConsumer,
+		sagaCommandProducer:    sagaCommandProducer,
+	}
+}
+
+func (f *SagaManagerFactory) Make(saga Saga) SagaManager {
+	return NewSagaManager(
+		saga,
+		f.sagaInstanceRepository,
+		f.commandProducer,
+		f.messageConsumer,
+		f.sagaCommandProducer,
+	)
+}
