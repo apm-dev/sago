a7d0346330df78bf195dc06f63de56809f7c7756
A.Parsa <develop.apm@gmail.com>
2021-07-06 19:52:38 +0430

    Refactor

--- command.go
+++ command.go
@@ -2,15 +2,21 @@ package sago
 
 import (
 	"apm-dev/sago/commands"
-
-	"google.golang.org/protobuf/proto"
 )
 
 type Command struct {
 	commands.Command
 	Name         string
 	Channel      string
-	Payload      []proto.Message
+	Payload      []byte
 	ReplyStructs []interface{}
 	ExtraHeaders map[string]string
+}
+
+func (c *Command) GetPayload() []byte {
+	return c.Payload
+}
+
+func (c *Command) GetName() string {
+	return c.Name
 }
--- commands/command.go
+++ commands/command.go
@@ -1,5 +1,7 @@
 package commands
 
+import "apm-dev/sago/messaging"
+
 type CommandReplyOutcome string
 
 const (
@@ -8,4 +10,15 @@ const (
 )
 
 type Command interface {
+	GetName() string
+	GetPayload() []byte
+}
+
+func MakeMessage(channel, replyTo string, cmd Command, headers map[string]string) messaging.Message {
+	b := messaging.WithPayload(cmd.GetPayload())
+	b.WithExtraHeaders("", headers)
+	b.WithHeader(DESTINATION, channel)
+	b.WithHeader(COMMAND_TYPE, cmd.GetName())
+	b.WithHeader(REPLY_TO, replyTo)
+	return b.Build()
 }
