6f8f792b8cdb90cc45667e0f11d02023cb133041
A.Parsa <develop.apm@gmail.com>
2021-08-21 10:58:21 +0430

    force command providers to return proto message

--- step_builder.go
+++ step_builder.go
@@ -1,5 +1,7 @@
 package sago
 
+import "google.golang.org/protobuf/proto"
+
 type StepBuilder struct {
 	parent *SagaDefinitionBuilder
 }
@@ -8,6 +10,6 @@ func NewStepBuilder(sdb *SagaDefinitionBuilder) *StepBuilder {
 	return &StepBuilder{sdb}
 }
 
-func (b *StepBuilder) InvokeParticipant(ce CommandEndpoint, cmdProvider func(sagaData []byte, vars map[string]interface{}) ([]byte, error)) *ParticipantInvocationStepBuilder {
+func (b *StepBuilder) InvokeParticipant(ce CommandEndpoint, cmdProvider func(sagaData []byte, vars map[string]interface{}) (proto.Message, error)) *ParticipantInvocationStepBuilder {
 	return NewParticipantInvocationStepBuilder(b.parent).withAction(ce, cmdProvider)
 }
